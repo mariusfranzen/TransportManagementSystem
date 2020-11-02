01fb74edc92f15adb7fc70180952c994fbb81822
Marius Franzén <marius.franzen1@gmail.com>
2020-11-02 11:28:56 +0100

    Encrypts password

--- src/main/java/database/User.java
+++ src/main/java/database/User.java
@@ -1,5 +1,16 @@
 package database;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Arrays;
+
 /**
  * @author Marius on 2020-11-02.
  * @version %I%, %G%
@@ -10,4 +21,46 @@ public class User {
     private String email;
     private String password;
 
+    public DBObject getAsDBObject(){
+        return new BasicDBObject()
+                .append("username", this.username)
+                .append("email", this.email)
+                .append("password", this.password);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public User setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public User setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public User setPassword(String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
+
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 128);
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        byte[] hash = factory.generateSecret(spec).getEncoded();
+
+        this.password = Arrays.toString(hash);
+        return this;
+    }
 }
