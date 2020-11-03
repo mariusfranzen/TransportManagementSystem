2b4b709ddf9e1cf251aad1ce7301dfc36468d1f6
Marius Franzén <marius.franzen1@gmail.com>
2020-11-03 12:16:51 +0100

    Updates

--- src/main/java/customer/Ticket.java
+++ src/main/java/customer/Ticket.java
@@ -3,17 +3,54 @@ package customer;
 import enums.TicketType;
 import goods.Person;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 /**
  * @author Marius on 2020-11-02.
  * @version %I%, %G%
  */
+
 public class Ticket {
 
     private UUID ticketNumber;
     private TicketType type;
-    private Person[] people;
-    private float price;
+    private List<Person> personList = new ArrayList<>();
+    private float price; //Euro
 
+    public Ticket(){
+        ticketNumber = UUID.randomUUID();
+    }
+
+    public UUID getTicketNumber() {
+        return ticketNumber;
+    }
+
+    public TicketType getType() {
+        return type;
+    }
+
+    public Ticket setType(TicketType type) {
+        this.type = type;
+        return this;
+    }
+
+    public List<Person> getPersonList() {
+        return personList;
+    }
+
+    public Ticket setPersonList(List<Person> personList) {
+        this.personList = personList;
+        return this;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public Ticket setPrice(float price) {
+        this.price = price;
+        return this;
+    }
 }
--- src/main/java/goods/Person.java
+++ src/main/java/goods/Person.java
@@ -4,6 +4,7 @@ package goods;
  * @author Marius on 2020-11-02.
  * @version %I%, %G%
  */
+
 public class Person {
 
     private String firstName;
@@ -14,4 +15,66 @@ public class Person {
     public String city;
     public String email;
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public Person setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Person setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public int getPinCode() {
+        return pinCode;
+    }
+
+    public Person setPinCode(int pinCode) {
+        this.pinCode = pinCode;
+        return this;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public Person setCountry(String country) {
+        this.country = country;
+        return this;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public Person setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+        return this;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public Person setCity(String city) {
+        this.city = city;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Person setEmail(String email) {
+        this.email = email;
+        return this;
+    }
 }
