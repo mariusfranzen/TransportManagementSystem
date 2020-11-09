bb0812950569ec2f5f87c2953561ee142db2b78b
Marius Franzén <marius.franzen1@gmail.com>
2020-11-09 12:51:23 +0100

    Getters and setters for Goods.java

--- src/main/java/goods/Goods.java
+++ src/main/java/goods/Goods.java
@@ -1,5 +1,7 @@
 package goods;
 
+import customer.Customer;
+
 /**
  * @author Marius on 2020-11-02.
  * @version %I%, %G%
@@ -8,5 +10,73 @@ public class Goods {
 
     private String description;
     private float weight;
+    private float width;
+    private float length;
+    private float height;
 
+    public float getLength() {
+        return length;
+    }
+
+    public Goods setLength(float length) {
+        this.length = length;
+        return this;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public Goods setWidth(float width) {
+        this.width = width;
+        return this;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public Goods setHeight(float height) {
+        this.height = height;
+        return this;
+    }
+
+    private Customer sender;
+    private Customer receiver;
+
+    public Customer getSender() {
+        return sender;
+    }
+
+    public Goods setSender(Customer sender) {
+        this.sender = sender;
+        return this;
+    }
+
+    public Customer getReceiver() {
+        return receiver;
+    }
+
+    public Goods setReceiver(Customer receiver) {
+        this.receiver = receiver;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Goods setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public Goods setWeight(float weight) {
+        this.weight = weight;
+        return this;
+    }
 }
